24636ef5315a89e59e31519d27578a396dbbb4d3
tafox-cap <59628730+tafox-cap@users.noreply.github.com>
2023-12-01 13:30:25 -0600

    Update update-state.php

--- update-state.php
+++ update-state.php
@@ -16,25 +16,3 @@ switch ($command) {
         echo "Race ended.";
         break;
     default:
-        // HTML form for testing purposes
-?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Race Simulation</title>
-</head>
-<body>
-  <h1>Race Simulation</h1>
-  <form action="update-state.php" method="get">
-    <label for="user">Username:</label>
-    <input type="text" id="user" name="user" required>
-    <button type="submit" name="command" value="addParticipant">Join Race</button>
-  </form>
-</body>
-</html>
-<?php
-        break;
-}
-?>
